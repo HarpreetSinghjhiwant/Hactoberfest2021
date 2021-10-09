91940c89e16884bbdf90e24d3d0025ec56aee7b0
HarpreetSinghjhiwant <92213246+HarpreetSinghjhiwant@users.noreply.github.com>
2021-10-09 17:36:33 +0530

    #hacktoberfest2021

--- simplecalculator.py
+++ simplecalculator.py
@@ -6,31 +6,31 @@ def add(x, y):
 
 # This function subtracts two numbers
 def subtract(x, y):
-    return x - y
+    return x-y
 
 # This function multiplies two numbers
 def multiply(x, y):
-    return x * y
+    return x*y
 
 # This function divides two numbers
 def divide(x, y):
-    return x / y
+    return x/y
 
 
 print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("1 = Add")
+print("2 = Subtract")
+print("3 = Multiply")
+print("4 = Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4):")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter first number:"))
+        num2 = float(input("Enter second number:"))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
@@ -51,4 +51,4 @@ while True:
           break
     
     else:
-        print("Invalid Input")
+        print("OH NO INVALID INPUT😱")
